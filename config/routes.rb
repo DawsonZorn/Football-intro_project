5eec26229eb275cd9432ddd476de059b231acf13
DawsonZorn <dawsonzorn@outlook.com>
2024-10-30 23:05:16 -0500

    Created routes for each division

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+  resources :leagues do #defines a route for each division and adds the search functionality
+    resources :seasons, only: [:show]
+    collection do
+      get 'search'
+    end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.

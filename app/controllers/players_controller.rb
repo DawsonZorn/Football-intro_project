72d59e0e186d2dcd60e679f3baec6edc68bc3e9d
DawsonZorn <dawsonzorn@outlook.com>
2024-10-31 13:41:35 -0500

    Added a hierarchical search to query the search with a filter on position and team

--- app/controllers/players_controller.rb
+++ app/controllers/players_controller.rb
@@ -1,6 +1,8 @@
 class PlayersController < ApplicationController
     def index
       @players = Player.includes(:team, :position, :coach, :competitions).page(params[:page]).per(10) # Display 10 players per page
+      @teams = Team.all # Make sure @teams is set to avoid map being undefined
+      @positions = Position.all # Make sure @positions is set
     end
 
   def show
@@ -8,11 +10,22 @@ class PlayersController < ApplicationController
   end
 
   def search
+    @teams = Team.all
+    @positions = Position.all
     if params[:query].present?
       @players = Player.includes(:position, :team, :coach, :competitions)
                         .where("players.name LIKE ?", "%#{params[:query]}%").page(params[:page]).per(10)
     else
       @players = Player.includes(:position, :team, :coach, :competitions).all.page(params[:page]).per(10)
+    end
+     # Filter by team
+     if params[:team_id].present?
+      @players = @players.where(team_id: params[:team_id])
+     end
+
+    # Filter by position
+    if params[:position_id].present?
+      @players = @players.where(position_id: params[:position_id])
     end
     render :index # Render the index view after searching
   end

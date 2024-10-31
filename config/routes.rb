870d86fb74076ce8d27628a6d3d657e30426e1ff
DawsonZorn <dawsonzorn@outlook.com>
2024-10-31 01:19:25 -0500

    updated my routes to access countries, leagues,seasons and matches

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,13 @@
 Rails.application.routes.draw do
-  resources :leagues do # defines a route for each division and adds the search functionality
-    resources :seasons, only: [ :show ]
-    collection do
-      get "search"
+  resources :countries, only: [ :show ] do
+    resources :leagues, only: [ :show ] do
+      resources :seasons, only: [ :show ] do
+        resources :matches, only: [ :index, :show ]
+      end
     end
+  end
+
+  get "/search", to: "matches#search", as: "search_matches"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
@@ -16,5 +20,4 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "posts#index"
-end
 end
